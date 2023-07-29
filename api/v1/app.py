14377d1f36b52e82820567e6c9143e2996fdeb30
6godpro <erickson5115@gmail.com>
2023-07-29 11:15:28 +0100

    return 404 status code now

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,22 +6,22 @@ create app error handlers and tear downs
 import os
 import models
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+print(app.url_map)
 
 @app.errorhandler(404)
 def page_not_found(error):
     """404 Error Handler"""
-    return jsonify({"error": "Not found"})
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.errorhandler(405)
 def not_allowed(error):
     """405 Error Handler"""
-    return jsonify({"error": "Request not allowed"})
+    return make_response(jsonify({"error": "Request not allowed"}), 405)
 
 
 @app.errorhandler(400)
